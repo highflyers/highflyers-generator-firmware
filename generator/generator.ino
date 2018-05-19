78d33b3ddd32894d51d379b914bcd7525ca44de2
maciejgaleja <maciej.galeja@o2.pl>
2018-05-19 20:52:22 +0200

    Added input filter and implemented output set

--- generator/generator.ino
+++ generator/generator.ino
@@ -5,8 +5,10 @@ Servo servo;
 int t1, t2, t3, vout;
 uint8_t dummy_load_current;
 int dummy_load_value;
-int servo_value;
+uint32_t debug_servo_value;
 
+uint32_t vout_iir_coeff = 210;
+uint32_t vout_prev = 0, vout_current = 0;
 
 int servo_dir = 1;
 
@@ -16,6 +18,24 @@ void read_analog_in()
   t1 = analogRead(A1);
   t2 = analogRead(A2);
   t3 = analogRead(A3);
+}
+
+void apply_vout_filter()
+{
+  vout_current = (vout_prev * vout_iir_coeff) + (vout * (256 - vout_iir_coeff));
+  vout_current /= 256;
+  vout_prev = vout_current;
+}
+
+uint32_t vout_get()
+{
+  return vout_current;
+}
+
+void output_set(uint32_t value)
+{
+  debug_servo_value = (value * 180) / 1024;
+  servo.write(debug_servo_value);
 }
 
 void dummy_load_write()
@@ -28,15 +48,15 @@ void dummy_load_write()
 
 void print_all()
 {
-  Serial.print(vout);
+  Serial.print(vout_get());
   Serial.print(" ");
-  Serial.print(dummy_load_current*16);
+  Serial.print(debug_servo_value);
   Serial.print(" ");
-  Serial.print(digitalRead(9)*255);
   Serial.println(" ");
 }
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
   //while (!Serial);
   pinMode(9, OUTPUT);
@@ -46,16 +66,14 @@ void setup() {
 void loop() {
   read_analog_in();
 
+  apply_vout_filter();
+
   ++dummy_load_value;
   if(dummy_load_value > 25500) dummy_load_value = 0;
   
-  servo_value += servo_dir;
-  if(servo_value > 160) servo_dir = -1;
-  if(servo_value < 30) servo_dir = 1;
-  
   delay(10);
 
-  servo.write(servo_value % 180);
+  output_set(vout_get());
   dummy_load_write();
 
   print_all();
